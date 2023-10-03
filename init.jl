9b7dbb6b4f71632669912691994a824d3e10de2b
Caroline G <124557580+PollyN0mial@users.noreply.github.com>
2023-10-03 08:58:24 +0200

    Added file for necessary packages

---
+++ init.jl
@@ -0,0 +1,15 @@
+import Pkg
+
+# Programming languages
+Pkg.add("JuMP")
+Pkg.add("MiniZinc")
+Pkg.add("PDDL")
+
+
+# Solvers
+Pkg.add("Cbc")
+
+# Other useful packages
+Pkg.add("CSV")
+Pkg.add("UnPack")
+Pkg.add("DataFrames")
