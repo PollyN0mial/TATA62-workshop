ff09f3ecae7dc609abcca99aaffdd24986d29741
caroline <caroline@example.com>
2023-10-03 11:45:50 +0200

    Updated the CP model for the knapsack problem

--- knapsack_problem/knapsack_CP.jl
+++ knapsack_problem/knapsack_CP.jl
@@ -1,10 +1,38 @@
-using JuMP
-import MiniZinc
+using JuMP, ConstraintSolver, UnPack
+const CS = ConstraintSolver
+
+include("import_data.jl")
 
 
-model = Model(() -> MiniZinc.Optimizer{Float64}("highs"))
-@variable(model, 1 <= x[1:3] <= 3, Int)
-@constraint(model, x in MOI.AllDifferent(3))
-@objective(model, Max, sum(i * x[i] for i in 1:3))
-optimize!(model)
-@show value.(x)
+"""
+    solveCP(file)
+
+Solves a knapsack problem using CP based on the data provided in "file"
+"""
+function solveCP(file="instances/knapPI_11_20_1000_1.csv")
+    
+    # Import the data
+    sets, params = import_data(file)
+
+    # Unpack the tuples
+    @unpack ITEMS = sets
+    @unpack profit,weight,max_weight = params
+
+    # Create the model
+    m = Model(CS.Optimizer) 
+
+    # Add variables
+    @variable(m, x[ITEMS], Bin)
+
+    # Add the knapsack constraint
+    @constraint(m, sum(weight[i]*x[i] for i in ITEMS) <=  max_weight)
+
+    # Add the objective function
+    @objective(m, Max, sum(profit[i]*x[i] for i in ITEMS))
+
+    # Solve
+    optimize!(m)
+    status = JuMP.termination_status(m)
+end
+
+solveCP()
