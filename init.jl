ab7b31de6d0cd90dd3f874452fa015b62c0fb49c
Caroline G <124557580+PollyN0mial@users.noreply.github.com>
2023-10-10 15:56:48 +0200

    Added comment to adress compatibility issues with the packages MiniZinc and ConstraintSolver

--- init.jl
+++ init.jl
@@ -16,3 +16,8 @@ Pkg.add("SymbolicPlanners") # library of PDDL solvers
 Pkg.add("CSV")
 Pkg.add("UnPack")
 Pkg.add("DataFrames")
+
+# NOTE: ConstraintSolver and MiniZinc seem to have compatibility issues. 
+# If you get an error while installing one of them, try removing the other one first:
+# Pkg.rm("MiniZinc")
+# Pkg.rm("ConstraintSolver")
